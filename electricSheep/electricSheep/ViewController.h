5af23b6af6d2460bf0b2f9c593cd7f7ff31f4ad1
Ali Helmy <alihelmy@gmail.com>
2012-10-23 15:35:20 +0200

    creating Xcode project

---
+++ electricSheep/electricSheep/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  electricSheep
+//
+//  Created by Ali Helmy on 23/10/2012.
+//  Copyright (c) 2012 wackyCube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ electricSheep/electricSheep/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  electricSheep
+//
+//  Created by Ali Helmy on 23/10/2012.
+//  Copyright (c) 2012 wackyCube. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
