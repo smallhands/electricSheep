2d0d29ee780ef752d1ee78c8af454b6d578395d3
Ali Helmy <alihelmy@gmail.com>
2012-10-30 18:35:00 +0200

    removed unnecessary viewport call

--- electricSheep.cpp
+++ electricSheep.cpp
@@ -73,8 +73,6 @@ void ElectricSheepEngine::freeResources() {
 }
 
 void ElectricSheepEngine::render() {
-    glViewport(0, 0, windowWidth, windowHeight);
-    
     //clear screen
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
