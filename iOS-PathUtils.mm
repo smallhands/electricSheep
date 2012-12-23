bcf0d0fbc47cb5a61ee28c550bb0f8f3a0a5d414
Ali Helmy <alihelmy@gmail.com>
2012-12-23 15:33:22 +0200

    complete implementation for dir

--- iOS-PathUtils.mm
+++ iOS-PathUtils.mm
@@ -8,6 +8,12 @@
 
 #include "Path_C_Interface.h"
 
-const char * pathForFile(const char *name, const char *extension) {
-    return [[[NSBundle mainBundle] pathForResource:[NSString stringWithUTF8String:name] ofType:[NSString stringWithUTF8String:extension]] UTF8String];
+const char * pathForFile(const char *name, const char *extension, const char *inDir) {
+    const char *path;
+    if (inDir!=nil) {
+        path = [[[NSBundle mainBundle] pathForResource:[NSString stringWithUTF8String:name] ofType:[NSString stringWithUTF8String:extension] inDirectory:[NSString stringWithUTF8String:inDir]] UTF8String];
+    } else {
+        path = [[[NSBundle mainBundle] pathForResource:[NSString stringWithUTF8String:name] ofType:[NSString stringWithUTF8String:extension]] UTF8String];
+    }
+    return path;
 }
