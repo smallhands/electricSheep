ab0c50daa60cb946728fa2cb4ff96a2e1355fd97
Ali Helmy <alihelmy@gmail.com>
2012-12-25 20:42:56 +0200

    vertex normal calculations

--- CubeModel.cpp
+++ CubeModel.cpp
@@ -49,10 +49,6 @@ CubeModel::CubeModel() : Model() {
         {{ 1.0,  1.0,  1.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 0.0}},
     };
     
-    for (int vi=0; vi<4*6; vi++) {
-        modelDataMap.push_back(data[vi]);
-    }
-    
     GLushort cubeFaces[] = {
         // front
         0,  1,  2,
@@ -74,7 +70,33 @@ CubeModel::CubeModel() : Model() {
         22, 23, 20,
     };
     
-    for (int fi=0; fi<6*6; fi++) {
+    //calculate models for vertices [FLAT SHADING]
+    for (int faceIndex=0; faceIndex<12; faceIndex++) {
+        GLushort vi0, vi1, vi2;
+        vi0 = cubeFaces[(faceIndex*3)+0];
+        vi1 = cubeFaces[(faceIndex*3)+1];
+        vi2 = cubeFaces[(faceIndex*3)+2];
+        
+        glm::vec3 v0 = glm::vec3(data[vi0].coord3D[0], data[vi0].coord3D[1], data[vi0].coord3D[2]);
+        glm::vec3 v1 = glm::vec3(data[vi1].coord3D[0], data[vi1].coord3D[1], data[vi1].coord3D[2]);
+        glm::vec3 v2 = glm::vec3(data[vi2].coord3D[0], data[vi2].coord3D[1], data[vi2].coord3D[2]);
+        
+        glm::vec3 v01 = v1 - v0;
+        glm::vec3 v12 = v2 - v1;
+        
+        glm::vec3 normal = glm::cross(v01, v12);
+        normal = glm::normalize(normal);
+        
+        data[vi0].normal[0] = data[vi1].normal[0] = data[vi2].normal[0] = normal.x;
+        data[vi0].normal[1] = data[vi1].normal[1] = data[vi2].normal[1] = normal.y;
+        data[vi0].normal[2] = data[vi1].normal[2] = data[vi2].normal[2] = normal.z;
+    }
+    
+    for (int vi=0; vi<24; vi++) {
+        modelDataMap.push_back(data[vi]);
+    }
+    
+    for (int fi=0; fi<36; fi++) {
         faces.push_back(cubeFaces[fi]);
     }
     
