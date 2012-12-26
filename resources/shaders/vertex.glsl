8280e9a7d5cd8ad74be544f79d775267624b4b1e
Ali Helmy <alihelmy@gmail.com>
2012-12-26 19:08:56 +0200

    quick ambient light

--- resources/shaders/vertex.glsl
+++ resources/shaders/vertex.glsl
@@ -10,14 +10,20 @@ uniform mat4 projection;
 
 varying lowp vec3 fragColor;
 
-struct lightSource
-{
-  vec4 position;
-  vec4 diffuse;
+struct lightSource {
+    vec4 position;
+    vec4 diffuse;
 };
 lightSource light0 = lightSource(
     vec4(1.0, 1.0, 1.0, 0.0),
     vec4(1.0, 1.0, 1.0, 1.0)
+);
+
+struct ambientLight {
+    vec4 diffuse;
+};
+ambientLight ambientLight0 = ambientLight(
+    vec4(0.3, 0.3, 0.3, 1.0)
 );
 
 void main(void){
@@ -25,7 +31,8 @@ void main(void){
 
     vec3 lightDirection = normalize(vec3(light0.position));
 
-    fragColor = (vec3(light0.diffuse) * vertexColour * max(0.0, dot(normalDirection, lightDirection))) + vec3(vertexColour.x*0.1, vertexColour.y*0.1, vertexColour.z*0.1);
+    fragColor = vec3(light0.diffuse) * vertexColour * max(0.0, dot(normalDirection, lightDirection));
+    fragColor += vec3(vertexColour.x*ambientLight0.diffuse.x, vertexColour.y*ambientLight0.diffuse.y, vertexColour.z*ambientLight0.diffuse.z);
 
     mat4 mvp = projection * view * model;
     gl_Position = mvp * vec4(coord3D, 1.0);
