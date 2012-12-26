eb4b2a81cc3dc145dc59cdb72714b3c8545f40d2
Ali Helmy <alihelmy@gmail.com>
2012-12-26 15:13:17 +0200

    transform normals from model space to world space

--- resources/shaders/vertex.glsl
+++ resources/shaders/vertex.glsl
@@ -10,6 +10,10 @@ varying lowp vec3 fragColor;
 
 void main(void){
    mat4 mvp = projection * view * model;
-   gl_Position = mvp*vec4(coord3D, 1.0);
-   fragColor = vertexColour+normal;
+
+   gl_Position = mvp * vec4(coord3D, 1.0);
+   
+   vec4 worldspaceNormal = mvp * vec4(normal, 1.0);
+
+   fragColor = vec3(vertexColour.x + worldspaceNormal.x, vertexColour.y + worldspaceNormal.y, vertexColour.z + worldspaceNormal.z);
 }
