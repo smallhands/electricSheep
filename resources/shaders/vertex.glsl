cbadddc06aaecb4b3f9c81f58c2966d1f5b5df44
Ali Helmy <alihelmy@gmail.com>
2012-12-26 19:05:29 +0200

    ambient lighting

--- resources/shaders/vertex.glsl
+++ resources/shaders/vertex.glsl
@@ -22,8 +22,10 @@ lightSource light0 = lightSource(
 
 void main(void){
     vec3 normalDirection = vec3(normalize(inverseTransposeModel * vec4(normal, 1.0)));
+
     vec3 lightDirection = normalize(vec3(light0.position));
-    fragColor = vec3(light0.diffuse) * vertexColour * max(0.0, dot(normalDirection, lightDirection));
+
+    fragColor = (vec3(light0.diffuse) * vertexColour * max(0.0, dot(normalDirection, lightDirection))) + vec3(vertexColour.x*0.1, vertexColour.y*0.1, vertexColour.z*0.1);
 
     mat4 mvp = projection * view * model;
     gl_Position = mvp * vec4(coord3D, 1.0);
