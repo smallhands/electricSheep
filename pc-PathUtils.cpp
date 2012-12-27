d2ffe7788218444320659a19816363cdf9f804a7
Ali Helmy <alihelmy@gmail.com>
2012-12-27 16:20:54 +0200

    better path lookup on windows

--- pc-PathUtils.cpp
+++ pc-PathUtils.cpp
@@ -1,12 +1,26 @@
 #include "Path_C_Interface.h"
 #include <string>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 using namespace std;
 
 const char * pathForFile(const char *name, const char *extension) {
-	string nameStr = string(name);
+    string pathPrefix = string("");
+    
+#ifdef _WIN32
+    char executablePath[1024] = {'\0'};
+    DWORD charsCopied = GetModuleFileName(NULL, executablePath, 1024);
+    if(charsCopied > 0 && charsCopied < 1024) {
+        pathPrefix = string(string(executablePath) + "\\..\\");
+    }
+#endif
+    
+    string nameStr = string(name);
 	string extStr = string(extension);
-	string *path = new string(nameStr + "." + extStr);
+	string *path = new string(pathPrefix + nameStr + "." + extStr);
 
 	return path->c_str();
 }
