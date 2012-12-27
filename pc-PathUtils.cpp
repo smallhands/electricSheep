a1ca5c4953594c73e6617140f84f1a09b3e96164
A. Helmy <alihelmy@gmail.com>
2012-12-28 01:15:31 +0200

    const path size

--- pc-PathUtils.cpp
+++ pc-PathUtils.cpp
@@ -11,7 +11,7 @@ const char * pathForFile(const char *name, const char *extension) {
     string pathPrefix = string("");
     
 #ifdef _WIN32
-    short pathSize = 1024;
+    const short pathSize = 1024;
     char executablePath[pathSize] = {'\0'};
     DWORD charsCopied = GetModuleFileName(NULL, executablePath, pathSize);
     if(charsCopied > 0 && charsCopied < pathSize) {
